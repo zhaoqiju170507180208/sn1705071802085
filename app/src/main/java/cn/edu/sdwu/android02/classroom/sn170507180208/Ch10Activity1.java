d281901baf1f81255955e486145d9b18d226d9df
zhaoqiju170507180208 <906721405@qq.com>
2020-04-21 10:19:57 +0800

    android02.classroom

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180208/Ch10Activity1.java
@@ -0,0 +1,13 @@
+package cn.edu.sdwu.android02.classroom.sn170507180208;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch10Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch10_1);
+    }
+}
